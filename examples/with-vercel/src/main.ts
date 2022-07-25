215d8b127dfb2124d51a748755af6261b2b1d0b4
Mikkel RINGAUD <mikkel@milescode.dev>
2022-07-26 00:53:43 +0200

    fix(examples/vercel): url link to api route

--- examples/with-vercel/src/main.ts
+++ examples/with-vercel/src/main.ts
@@ -5,7 +5,7 @@ app.innerHTML = `
   <h1>Hello World, Vercel!</h1>
   <p>
     This is the / (home) route.
-    Maybe you want to <a href="/api">go to the /api route</a> ?  
+    Maybe you want to <a href="/api/fetch">go to the /api/fetch route</a> ?  
   </p>
 `;
 
@@ -14,7 +14,7 @@ const fetch_time = async () => {
   const time_data = await ky("/api/time").json() as { time: string };
   app.innerHTML += `
     <p>
-      The time from the API route is: ${time_data.time}
+      The time from the <a href="/api/time">/api/time</a> route is: ${time_data.time}
     </p>
   `;
 };
